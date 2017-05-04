866eb889eae733c2515541825c1b70a2ffc58628
Dani Carrion <daniel@cartodb.com>
2017-05-04 23:21:11 +0200

    new layout and 2 examples

---
+++ functions/drop_pepsicocas.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION {schema}.drop_pepsicocas()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+    row     record;
+BEGIN
+    FOR row IN
+        select pg_class.relname from pg_class, pg_roles, pg_namespace where pg_roles.oid = pg_class.relowner and pg_roles.rolname = current_user and pg_namespace.oid = pg_class.relnamespace and pg_class.relkind = 'r' and pg_class.relname like 'pepsicoca%' and pg_class.relname not like 'pepsicoca\_1%'
+    LOOP
+        EXECUTE 'DROP TABLE ' || quote_ident(row.relname) || ' CASCADE';
+        RAISE INFO 'Dropped table: %', quote_ident(row.relname);
+    END LOOP;
+END;
+$function$
